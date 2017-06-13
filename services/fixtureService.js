9008312efbd702b88abfeb3797b82c8e1099c61b
francoramiro7 <francoramiro7@gmail.com>
2017-06-13 02:14:34 -0300

    Se agregan los campos cancha y hora a los partidos

--- services/fixtureService.js
+++ services/fixtureService.js
@@ -6,6 +6,7 @@ var Partido = mongoose.model('Partido');
 var Equipo = mongoose.model('Equipo');
 var Torneo = mongoose.model('Torneo');
 var Division = mongoose.model('Division');
+var Cancha = mongoose.model('Cancha');
 var logger = require('../logger');
 
 //GET - Return all partidos in the DB
@@ -14,36 +15,48 @@ exports.getPartidos = function(req, res) {
     Partido.find({'division': mongoose.Types.ObjectId(req.params.id)}, function (err, partidos) {
         if (err) return res.send(500, err.message);
         Equipo.find(function(err, equipos) {
-            var dictionary =  {};
+            Cancha.find(function (err,canchas) {
+                var dictionary =  {};
 
-            for(var i = 0; i< partidos.length;i++){
-                if (dictionary[partidos[i].fecha_numero] == null) {
-                    dictionary[partidos[i].fecha_numero] = new Array();
-                }
-                var obj = {};
-                var fecha =  partidos[i].fecha;
-
-                obj["fecha"] = (('0' + (fecha.getDate())).slice(-2)
-                + '/' + ('0' + (fecha.getMonth() + 1)).slice(-2) + "\n"+
-                    ('0' + (fecha.getHours())).slice(-2) + ":" +
-                    ('0' + (fecha.getMinutes())).slice(-2)
-                );
-                obj["estado"] = partidos[i].estado;
-                obj["cancha"] = partidos[i].cancha == null ? "" : partidos[i].cancha;
-                obj["marcador1"] = partidos[i].marcador_equipo_1 == null ? "" : partidos[i].marcador_equipo_1;
-                obj["marcador2"] = partidos[i].marcador_equipo_2 == null ? "" : partidos[i].marcador_equipo_2;
-
-                for(var o = 0 ; o<equipos.length;o++){
-                    if(equipos[o]._id.equals(partidos[i].equipo1)){
-                        obj["equipo1"] = equipos[o].nombre;
-                    } else if(equipos[o]._id.equals(partidos[i].equipo2)){
-                        obj["equipo2"] = equipos[o].nombre;
+                for(var i = 0; i< partidos.length;i++){
+                    if (dictionary[partidos[i].fecha_numero] == null) {
+                        dictionary[partidos[i].fecha_numero] = new Array();
                     }
+                    var obj = {};
+                    var fecha =  partidos[i].fecha;
+
+                    obj["fecha"] = (('0' + (fecha.getDate())).slice(-2)
+                        + '/' + ('0' + (fecha.getMonth() + 1)).slice(-2) + "\n"+
+                        ('0' + (fecha.getHours())).slice(-2) + ":" +
+                        ('0' + (fecha.getMinutes())).slice(-2)
+                    );
+                    obj["estado"] = partidos[i].estado;
+                    obj["cancha"] = "";
+                    if(partidos[i].cancha != null) {
+                        for(var k = 0; k < canchas.length; k++ ){
+                            if(canchas[k]._id.equals(partidos[i].cancha)){
+                                obj["cancha"] = canchas[k].nombre;
+                            }
+                        }
+                    }
+
+
+                    obj["marcador1"] = partidos[i].marcador_equipo_1 == null ? "" : partidos[i].marcador_equipo_1;
+                    obj["marcador2"] = partidos[i].marcador_equipo_2 == null ? "" : partidos[i].marcador_equipo_2;
+
+                    for(var o = 0 ; o<equipos.length;o++){
+                        if(equipos[o]._id.equals(partidos[i].equipo1)){
+                            obj["equipo1"] = equipos[o].nombre;
+                        } else if(equipos[o]._id.equals(partidos[i].equipo2)){
+                            obj["equipo2"] = equipos[o].nombre;
+                        }
+                    }
+                    dictionary[partidos[i].fecha_numero].push(obj)
                 }
-                dictionary[partidos[i].fecha_numero].push(obj)
-            }
-            res.status(200).jsonp(dictionary);
+                res.status(200).jsonp(dictionary);
+            });
         });
+
     });
 
 };
--- views/js/equiposPorTorneo.js
+++ views/js/equiposPorTorneo.js
@@ -1,82 +1,108 @@
 $("#elegirEquipo1").hide();
 $("#elegirEquipo2").hide();
+$("#canchaTorneo").hide();
 $("#divisionesTorneo").hide();
 
 $( "#fecha" ).datepicker({
-      dateFormat: 'yy-mm-dd',
-      timezone: "UTC−4"
+    dateFormat: 'yy-mm-dd',
+    timezone: "UTC−4"
 });
 
 $("#torneoSelect").change(function(){
-	if($("#torneoSelect").val()=="none"){
-		$("#divisionesTorneo").hide();
-		$("#elegirEquipo1").hide();
-		$("#elegirEquipo2").hide();
-	} else {
-		$("#divisionesTorneo").show();
-		$("#divisionesTorneo").css("visibility","visible");
-		$("#divisionSelect").empty();
+    if($("#torneoSelect").val()=="none"){
+        $("#divisionesTorneo").hide();
+        $("#elegirEquipo1").hide();
+        $("#elegirEquipo2").hide();
+        $("#canchaTorneo").hide();
+    } else {
+        $("#divisionesTorneo").show();
+        $("#canchaTorneo").show();
+        $("#divisionesTorneo").css("visibility","visible");
+        $("#canchaTorneo").css("visibility","visible");
+        $("#divisionSelect").empty();
+        $("#canchaSelect").empty();
 
-		var idTorneo = $("#torneoSelect").val();
-		$.get('http://localhost:3000/division/torneo/'+idTorneo, function(divisiones) {
-			if ( divisiones.length == 0 ){
-				$("#divisionesTorneo").hide();
-				$("#elegirEquipo1").hide();
-				$("#elegirEquipo2").hide();
-			} else {
-				$("#divisionSelect").append('<option value="none" selected></option>');
-			  	for (var i = 0; i < divisiones.length; i++) {
-			  		$("#divisionSelect").append('<option value='+divisiones[i]._id+'>'+divisiones[i].nombre+'</option>');
-			  	};
-			}
+        var idTorneo = $("#torneoSelect").val();
+        $.get('http://localhost:3000/division/torneo/'+idTorneo, function(divisiones) {
+            $.get('http://localhost:3000/cancha/torneo/'+idTorneo, function(canchas) {
+                if ( divisiones.length == 0 ){
+                    $("#divisionesTorneo").hide();
+                    $("#elegirEquipo1").hide();
+                    $("#canchaTorneo").hide();
+                    $("#elegirEquipo2").hide();
+                } else {
+                    $("#divisionSelect").append('<option value="none" selected></option>');
+                    for (var i = 0; i < divisiones.length; i++) {
+                        $("#divisionSelect").append('<option value='+divisiones[i]._id+'>'+divisiones[i].nombre+'</option>');
+                    };
+                }
+                if ( canchas.length == 0 ){
+                    $("#canchaTorneo").hide();
+                } else {
+                    $("#canchaSelect").append('<option value="none" selected></option>');
+                    for (var n = 0; n < canchas.length; n++) {
+                        $("#canchaSelect").append('<option value='+canchas[n]._id+'>'+canchas[n].nombre+'</option>');
+                    };
+                }
 
-		});
-	}
+            });
+        });
+    }
 });
 
 $("#divisionSelect").change(function(){
-	if($("#divisionSelect").val()=="none"){
-		$("#elegirEquipo1").hide();
-		$("#elegirEquipo2").hide();
-		$("#submitForm").prop('disabled', true);
-	} else {
-		$("#submitForm").prop('disabled', false);
-		$('#equipo1Select').empty();
-		$('#equipo2Select').empty();
-		$("#elegirEquipo1").show();
-		$("#elegirEquipo1").css("visibility","visible");
-		$("#elegirEquipo2").show();
-		$("#elegirEquipo2").css("visibility","visible");
-		var idDivision = $("#divisionSelect").val();
-		$.get('http://localhost:3000/division/'+idDivision+'/equipos', function(equipos) {
-			if ( equipos.length == 0 ){
-				$("#elegirEquipo1").hide();
-				$("#elegirEquipo2").hide();
-				$("#submitForm").prop('disabled', true);
-			} else {
-			  	for (var i = 0; i < equipos.length; i++) {
-			  		$("#equipo1Select").append('<option value='+equipos[i]._id+'>'+equipos[i].nombre+'</option>');
-			  		$("#equipo2Select").append('<option value='+equipos[i]._id+'>'+equipos[i].nombre+'</option>');
-			  	};
-			}
-		});
-	}
+    if($("#divisionSelect").val()=="none"){
+        $("#elegirEquipo1").hide();
+        $("#elegirEquipo2").hide();
+        $("#submitForm").prop('disabled', true);
+    } else {
+        $("#submitForm").prop('disabled', false);
+        $('#equipo1Select').empty();
+        $('#equipo2Select').empty();
+        $("#elegirEquipo1").show();
+        $("#elegirEquipo1").css("visibility","visible");
+        $("#elegirEquipo2").show();
+        $("#elegirEquipo2").css("visibility","visible");
+        var idDivision = $("#divisionSelect").val();
+        $.get('http://localhost:3000/division/'+idDivision+'/equipos', function(equipos) {
+            if ( equipos.length == 0 ){
+                $("#elegirEquipo1").hide();
+                $("#elegirEquipo2").hide();
+                $("#submitForm").prop('disabled', true);
+            } else {
+                for (var i = 0; i < equipos.length; i++) {
+                    $("#equipo1Select").append('<option value='+equipos[i]._id+'>'+equipos[i].nombre+'</option>');
+                    $("#equipo2Select").append('<option value='+equipos[i]._id+'>'+equipos[i].nombre+'</option>');
+                };
+            }
+        });
+    }
 
 });
 
 $( "#submitForm" ).click(function(e){
-	e.preventDefault();
-	var equipo1 = $("#equipo1Select").val();
-	var equipo2 = $("#equipo2Select").val();
-	if (equipo1 == equipo2){
-		alert("Los equipos deben ser distintos");
-		return false;
-	}
-	var fecha = $("#fecha").val();
-	if (!fecha){
-		alert("Debe ingresar la fecha del partido");
-		return false;
-	}
-	$( "#formAgregarPartido" ).submit();
+    e.preventDefault();
+    var equipo1 = $("#equipo1Select").val();
+    var equipo2 = $("#equipo2Select").val();
+    if (equipo1 == equipo2){
+        alert("Los equipos deben ser distintos");
+        return false;
+    }
+    var fecha = $("#fecha").val();
+    if (!fecha){
+        alert("Debe ingresar la fecha del partido");
+        return false;
+    }
+    var hora = $("#hora").val();
+    if (!hora){
+        alert("Debe ingresar la hora del partido");
+        return false;
+    }
+    var cancha = $("#canchaSelect").val();
+    if(!cancha){
+        alert("Debe ingresar la cancha donde se juegará el partido");
+        return false;
+    }
+    $( "#formAgregarPartido" ).submit();
 });
 
